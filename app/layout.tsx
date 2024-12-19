f83f78324ee02b1b0b09e2bbe3fc0ead92e6f9de
Henry Levenberg <henrylevenberg@gmail.com>
2024-12-18 19:04:01 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Lisa-Marie Schwingshandl",
-    description: "Business Development Partner at the CSM Family Office",
+    title: "Henry Levenberg",
+    description: "Product Learning at SAP",
 };
 
 export default function RootLayout({
